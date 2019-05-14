e445e70dc2e52c8d0e19bd3c8be3333e2b1e6700
VikasiniN <spl.anand@gmail.com>
2019-05-14 15:23:44 +0530

    changes in config

--- src/config/appSetting.js
+++ src/config/appSetting.js
@@ -7,12 +7,12 @@ module.exports = {
     adsServerPath: 'http://localhost/SaifB2B/ads/',
     logoUploadPath: 'images/logo',
     logoServerPath: 'http://localhost/SaifB2B/logo/' */
-    productUploadPath: "/home/ubuntu/app/app/seller-ui/saif-seller/images/products",
+    productUploadPath: "/home/ubuntu/app/seller-ui/saif-seller/images/products",
     productServerPath: "http://sofacreator.com/admin/images/products/",
-    bannerUploadPath: "/home/ubuntu/app/app/seller-ui/saif-seller/images/banners",
+    bannerUploadPath: "/home/ubuntu/app/seller-ui/saif-seller/images/banners",
     bannerServerPath: "http://sofacreator.com/admin/images/banners/",
-    adsUploadPath: "/home/ubuntu/app/app/seller-ui/saif-seller/images/ads",
+    adsUploadPath: "/home/ubuntu/app/seller-ui/saif-seller/images/ads",
     adsServerPath: "http://sofacreator.com/admin/images/ads/",
-    logoUploadPath: "/home/ubuntu/app/app/seller-ui/saif-seller/images/logo",
+    logoUploadPath: "/home/ubuntu/app/seller-ui/saif-seller/images/logo",
     logoServerPath: "http://sofacreator.com/admin/images/logo/"
 };
